1682af4ef83a4872f22209cf0733c69af078459a
Yash Mahajan <ymahajan1612@gmail.com>
2025-01-07 19:31:52 +0000

    implemented the second seq2point model that uses LSTM layers

--- seq2Point_model.py
+++ seq2Point_model.py
@@ -53,4 +53,51 @@ class Seq2PointSimple(Seq2PointBase):
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+
+class Seq2PointLSTM(Seq2PointBase):
+    """
+    Seq2Point model with LSTM.
+    """
+
+    def __init__(self):
+        super(Seq2PointLSTM, self).__init__(input_window_length=599)
+
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=30, kernel_size=10, stride=1, padding='same')
+        self.conv2 = nn.Conv1d(in_channels=30, out_channels=30, kernel_size=8, stride=1, padding='same')
+        self.conv3 = nn.Conv1d(in_channels=30, out_channels=40, kernel_size=6, stride=1, padding='same')
+        self.dropout1 = nn.Dropout(0.2)
+
+        self.conv4 = nn.Conv1d(in_channels=40, out_channels=50, kernel_size=5, stride=1, padding='same')
+        self.dropout2 = nn.Dropout(0.2)
+
+        self.lstm1 = nn.LSTM(input_size=50, hidden_size=64, batch_first=True)
+        self.lstm2 = nn.LSTM(input_size=64, hidden_size=64, batch_first=True)
+
+        self.fc1 = nn.Linear(64, 1)
+
+        self.relu = nn.ReLU()
+
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.dropout1(x)
+
+        x = self.relu(self.conv4(x))
+        x = self.dropout2(x)
+
+        x = x.transpose(1, 2)
+
+        x, _ = self.lstm1(x)
+        x, _ = self.lstm2(x)
+
+        mid_index = x.size(1) // 2
+        x = x[:, mid_index, :]
+
+        x = self.fc1(x)
+
+        return x
     
+
