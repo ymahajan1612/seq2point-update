197922633b049d81ced1149e679c3cf7cc1c5aae
Yash Mahajan <ymahajan1612@gmail.com>
2025-03-05 21:07:40 +0000

    slidingwindowdataset now normalises the data

--- data_feeder.py
+++ data_feeder.py
@@ -21,7 +21,7 @@ class SlidingWindowDataset(Dataset):
             self.normalisation_params[file] = house_params
             inputs = torch.tensor(df.iloc[:, 1].values, dtype=torch.float32)
             outputs = torch.tensor(df.iloc[:, 2].values, dtype=torch.float32)
-            self.data.append((inputs, outputs))  # Store each house as a tuple
+            self.data.append((inputs, outputs, file))  # Store each house as a tuple along with the file name
 
     def __len__(self):
         return sum(len(inputs) - self.window_size + 1 for inputs, _ in self.data)
@@ -30,12 +30,19 @@ class SlidingWindowDataset(Dataset):
         return self.normalisation_params[file_dir]
 
     def __getitem__(self, idx):
-        for inputs, outputs in self.data:
+        for inputs, outputs, file in self.data:
             num_windows = len(inputs) - self.window_size + 1
             if idx < num_windows:
                 start_idx = idx
                 end_idx = idx + self.window_size
-                return inputs[start_idx:end_idx], outputs[start_idx + self.offset]
+                
+                norm_params = self.getNormalisationParams(file)
+
+                normalised_inputs = (inputs[start_idx:end_idx] - norm_params["aggregate_mean"]) / norm_params["aggregate_std"]
+                normalised_outputs = (outputs[start_idx:end_idx] - norm_params["appliance_mean"]) / norm_params["appliance_std"]
+
+
+                return normalised_inputs, normalised_outputs
             idx -= num_windows  # Move to the next house's data
 
         raise IndexError("Index out of range")
