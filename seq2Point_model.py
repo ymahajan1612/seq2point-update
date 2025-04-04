abf5b8b618e2ae028894785971faba1cb3c92359
Yash Mahajan <86207290+ymahajan1612@users.noreply.github.com>
2025-04-04 19:50:59 +0100

    Added batch normalisation to Seq2PointBalanced

--- seq2Point_model.py
+++ seq2Point_model.py
@@ -128,26 +128,46 @@ class Seq2PointCNNLSTM(Seq2PointBase):
 
 class Seq2PointBalanced(Seq2PointBase):
     """
-    Balanced Seq2Point model 
+    Balanced Seq2Point model with Batch Normalization.
     """
-
     def __init__(self, input_window_length=599):
-        super(Seq2PointBalanced, self).__init__(input_window_length=input_window_length)
+        super(Seq2PointBalanced, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(8, 1), stride=(1, 1), padding='same')
+        self.input_window_length = input_window_length
+
+        self.conv1 = nn.Conv2d(
+            in_channels=1,
+            out_channels=16,
+            kernel_size=(8, 1),
+            stride=(1, 1),
+            padding='same'
+        )
+        self.bn1 = nn.BatchNorm2d(16)
         self.pool1 = nn.MaxPool2d(kernel_size=(2, 1))
 
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=24, kernel_size=(6, 1), stride=(1, 1), padding='same')
-        self.pool2 = nn.MaxPool2d(kernel_size=(2, 1))  
+        self.conv2 = nn.Conv2d(
+            in_channels=16,
+            out_channels=24,
+            kernel_size=(6, 1),
+            stride=(1, 1),
+            padding='same'
+        )
+        self.bn2 = nn.BatchNorm2d(24)
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 1))
 
-        self.conv3 = nn.Conv2d(in_channels=24, out_channels=32, kernel_size=(5, 1), stride=(1, 1), padding='same')
-
-        self.pool3 = nn.MaxPool2d(kernel_size=(2, 1))  
+        self.conv3 = nn.Conv2d(
+            in_channels=24,
+            out_channels=32,
+            kernel_size=(5, 1),
+            stride=(1, 1),
+            padding='same'
+        )
+        self.bn3 = nn.BatchNorm2d(32)
+        self.pool3 = nn.MaxPool2d(kernel_size=(2, 1))
         self.dropout3 = nn.Dropout(0.3)
 
-
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(32 * (self.input_window_length // 8), 768)  
+        self.fc1 = nn.Linear(32 * (self.input_window_length // 8), 768)
         self.dropout_fc1 = nn.Dropout(0.3)
 
         self.fc2 = nn.Linear(768, 1)
@@ -156,18 +176,27 @@ class Seq2PointBalanced(Seq2PointBase):
 
     def forward(self, x):
         x = x.unsqueeze(1).unsqueeze(-1)
-        x = self.relu(self.conv1(x))
-        x = self.pool1(x)  
 
-        x = self.relu(self.conv2(x))
-        x = self.pool2(x)  
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.pool1(x)
 
-        x = self.relu(self.conv3(x))
-        x = self.pool3(x)  
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.pool2(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu(x)
+        x = self.pool3(x)
         x = self.dropout3(x)
 
         x = self.flatten(x)
-        x = self.relu(self.fc1(x))
+
+        x = self.fc1(x)
+        x = self.relu(x)
         x = self.dropout_fc1(x)
 
         x = self.fc2(x)
